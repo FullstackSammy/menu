5b0ec109c7dcf069db35a8817543a1a3ef8b721a
samzzii <nasstromsamuel@icloud.com>
2023-12-05 12:54:49 +0100

    added views and urls for detailview

--- menu/urls.py
+++ menu/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import item_list, item_list_serialized
+from .views import item_list, item_list_serialized, item_detail
 
 urlpatterns = [
     path('', item_list, name='item-list'),
     path('drf/', item_list_serialized, name='item-list-drf'),
+    path('drf/<int:pk>', item_detail),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 
+from rest_framework.response import Response
+
 from .serializers import ItemSerializer
 from .models import Item
 
@@ -21,4 +23,9 @@ def item_list_serialized(request):
     items = Item.objects.all()
     # Now we can serialize these using the serializer we just made
     serializer = ItemSerializer(items, many=True) #the serializer we just made. many is refering to if we have more than one item or not.
-    return JsonResponse(serializer.data, safe=False) # Using the sam JsonResponse class as before.
+    return Response(serializer.data) # Using the sam JsonResponse class as before.
+
+def item_detail(request, pk):
+    item = Item.objects.get(id=pk)
+    serializer = ItemSerializer(item)
+    return Response(serializer.data)
